b829af61b9619925502cffd415c067fd01624c95
willie891031 <willie891031@gmail.com>
2023-06-25 22:53:54 +0800

    第一版初始排成

---
+++ StockAPI/StockAPI/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Hangfire;
+using Hangfire.MemoryStorage;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(StockAPI.App_Start.Startup))]
+
+namespace StockAPI.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Hangfire.GlobalConfiguration.Configuration.UseMemoryStorage();
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
